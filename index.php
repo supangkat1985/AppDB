c88c5fc680d51c2cfe087e542c6cf2cb66b39ef6
supangkat1985 <52622637+supangkat1985@users.noreply.github.com>
2019-07-07 20:06:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:supangkatappserver.database.windows.net,1433; Database = dicodingdb", "supangkat@supangkatappserver", "Bangsat123");
+    $conn = new PDO("sqlsrv:server = tcp:supangkatappserver.database.windows.net,1433; Database = dicodingdb", "supangkat", "Bangsat123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
