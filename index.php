26b97403e8ba30ab101865200aefa9b08ac438c1
supangkat1985 <52622637+supangkat1985@users.noreply.github.com>
2019-07-07 20:20:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,6 +31,15 @@
     $pass = "Bangsat123";
     $db = "dicodingdb";
     
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:supangkatappserver.database.windows.net,1433; Database = dicodingdb", "supangkat", "Bangsat123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "supangkat@supangkatappserver", "pwd" => "Bangsat123", "Database" => "dicodingdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
