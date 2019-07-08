754abf265bc2d481ad449b81081bab880e48a4fe
supangkat1985 <52622637+supangkat1985@users.noreply.github.com>
2019-07-08 13:52:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -32,19 +32,19 @@
     $db = "dicodingdb";
     
 // PHP Data Objects(PDO) Sample Code:
-//try {
-//    $conn = new PDO("sqlsrv:server = tcp:supangkatappserver.database.windows.net,1433; Database = dicodingdb", "supangkat", "Bangsat123");
-//    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//}
-//catch (PDOException $e) {
-//    print("Error connecting to SQL Server.");
-//    die(print_r($e));
-//}
+try {
+    $conn = new PDO("sqlsrv:server = tcp:supangkatappserver.database.windows.net,1433; Database = dicodingdb", "supangkat", "Bangsat123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "supangkat@supangkatappserver", "pwd" => "Bangsat123", "Database" => "dicodingdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:supangkatappserver.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+//$connectionInfo = array("UID" => "supangkat@supangkatappserver", "pwd" => "Bangsat123", "Database" => "dicodingdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+//$serverName = "tcp:supangkatappserver.database.windows.net,1433";
+//$conn = sqlsrv_connect($serverName, $connectionInfo);
 
     if (isset($_POST['submit'])) {
         try {
