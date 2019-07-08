59ace643dcdef36fca18d78d5f1abf9ab1cde2d4
supangkat1985 <52622637+supangkat1985@users.noreply.github.com>
2019-07-08 14:37:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
 <?php
-    $host = "cp:supangkatappserver.database.windows.net,1433";
+    $host = "tcp:supangkatappserver.database.windows.net,1433";
     $user = "supangkat";
     $pass = "Bangsat123";
     $db = "dicodingdb";
